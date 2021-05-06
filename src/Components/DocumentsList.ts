e4fc273ebd26022809b36a62f9efaca64484d1d1
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-06 18:42:47 +0400

    ADD: fetch dummy

--- src/Components/DocumentsList.ts
+++ src/Components/DocumentsList.ts
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class DocumentsList extends React.Component {
+
+	state = {
+			error: null,
+			isLoaded: false,
+			items: Array		
+		}
+
+	componentDidMount() {
+	fetch("https://localhost:5001/api/v1/Document/names")
+		.then(response => response.json())
+		.then(response => {
+			this.setState({items: response});
+			this.setState({isLoaded: true});
+		})
+		.then(response => {
+			this.setState({false: true});
+			this.setState({Error});
+		})
+	}
+
 	render(){
-		return "<p>Document</p>";
+		console.log(this.state.items);
+		return "data";
 	}
 }
 
