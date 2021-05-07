8facb503d9a4a37a9e2d35a989f4bacafaecb677
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-07 17:24:25 +0400

    ADD: main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+//@ts-ignore
+import Main from './Pages/Main.tsx';
 // @ts-ignore
 import Manuscript from './Pages/Manuscript.tsx';
 // @ts-ignore
@@ -12,11 +14,15 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/manuscript/">Manuscripts</Link>
+            <Link to="/">Main page</Link>
           </li>
+          <li>
+            <Link to="/manuscript/">Manuscripts</Link>
+          </li>          
         </ul>
       </nav>
       <Route path="/manuscript/" component={Manuscript} />
+      <Route path="/" exact component={Main} />
     </Router>
   );
 }
