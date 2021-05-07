70d7a4aaf33a1cbefa00918083aeebe9f0f52746
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-07 16:22:17 +0400

    ADD: Manuscript page dummy

---
+++ src/Pages/Manuscript.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// @ts-ignore
+import DocumentsList from '../Components/DocumentsList.tsx';
+
+class Manuscript extends React.Component {
+	
+	render() {
+		return(
+		<div>
+			<DocumentsList />
+		</div>
+	);
+	}
+}
+
+export default Manuscript;
