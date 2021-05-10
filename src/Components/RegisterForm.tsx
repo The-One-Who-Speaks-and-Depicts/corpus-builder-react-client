c539186ffaf69e56c719b1a40d350a2c3ed99e6e
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-10 23:51:54 +0400

    EDIT: sign up results are shown directly

--- src/Components/RegisterForm.tsx
+++ src/Components/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React from 'react';
 //@ts-ignore
 import {register} from "../utils/register.tsx";
 
@@ -10,14 +10,41 @@ export function RegisterForm() {
 				Login: <input id="register_login" type="text" required /><br />
 				Password: <input id="register_password" type="password" required /><br />
 				Repeat password: <input id="register_repeated_password" type="password" required /><br />
-				<div id="response"></div>
+				<div id="register_response"></div><br />
 				<button onClick={async(e) => {
 					e.preventDefault(); 
 					let login = (document.getElementById("register_login") as HTMLInputElement).value;
 					let password = (document.getElementById("register_password") as HTMLInputElement).value;
 					let repetition = (document.getElementById("register_repeated_password") as HTMLInputElement).value; 
 					await register(login, password, repetition)
-					.then((value : any) => console.log(value));
+					.then((value : any) => {
+						let response = document.getElementById("register_response");
+						if (response != null) {
+							response.innerText = "";
+						}
+						if (value) {
+							if (response != null) {
+								response.innerText = "Successfully signed up!";
+							}
+						}
+						else {
+							if (response != null) {
+								response.innerText = "Some error occurred, please, try again";
+							}							 
+						}
+						let loginField = document.getElementById("register_login") as HTMLInputElement;
+						if (loginField != null) {
+							loginField.value = "";
+						}
+						let passwordField = document.getElementById("register_password") as HTMLInputElement;
+						if (passwordField != null) {
+							passwordField.value = "";
+						}
+						let repetitionField = document.getElementById("register_repeated_password") as HTMLInputElement;
+						if (repetitionField != null) {
+							repetitionField.value = "";
+						}
+					});
 				}}>
 				Register
 				</button>
