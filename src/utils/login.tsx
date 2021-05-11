1926c3e1f96d83404590450ed981826c7abb95e5
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-11 14:53:15 +0400

    ADD: login preliminary implementation

--- src/utils/login.tsx
+++ src/utils/login.tsx
@@ -1,7 +1,13 @@
-export const login = async() => {
-	return {
-		id: 4,
-		username: "Bob",
-		email: "bob@bobmail.com"
-	};	
+export const login = async(login: string, password: string) => {
+	const res : object | null = await fetch("http://localhost:5000/api/v1/User/?" + new URLSearchParams({login: login, password: password, repeatedPassword: repeatedPassword}), {method: 'POST'})
+		.then(response => response.text())
+		.then(response => {
+			if (response === "true") {
+				return  {
+					id: login
+				};
+			}
+			return null;
+		});
+	return res;	
 };
