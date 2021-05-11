ba85bfe559a8c3608aa09e0d003cdb5a0c2b924b
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-11 16:35:10 +0400

    ADD: authorization

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,20 +1,48 @@
 import React, {useContext} from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 //@ts-ignore
 import {UserContext} from "../UserContext.tsx";
 //@ts-ignore
 import {login} from "../utils/login.tsx";
-
+//@ts-ignore
+import Main from "../Pages/Main.tsx";
 
 export function LoginForm() {
 	const {user, setUser} = useContext(UserContext);
 	return (
 		<div>
-			<form action="post">
-				Login: <input type="text" required /><br />
-				Password: <input type="password" required /><br />
-				<button onClick={async(e) => {e.preventDefault();const user = await login; setUser(user);}}>Submit</button>
-			</form>
-			
-		</div>
-		);
+			{user 
+				? 
+					<Redirect to="/" />			
+				:
+					<form action="post">
+						Login: <input id="login" type="text" required /><br />
+						Password: <input id="password" type="password" required /><br />
+						<div id="login_response"></div><br />
+						<button onClick={async(e) => {
+							e.preventDefault();
+							let loginField = document.getElementById("login") as HTMLInputElement;
+							let passwordField = document.getElementById("password") as HTMLInputElement;
+							let response = document.getElementById("login_response");
+							if (response != null) {
+								response.innerText = "";
+							}					
+							const user = await login(loginField.value, passwordField.value); 
+							if (user != null) {
+								setUser(user);
+								if (response != null) {
+									response.innerText = "Sucessfully logged in!";
+								}
+							}
+							else {
+								if (response != null) {
+									response.innerText = "Error occured, please, try again";
+								}
+							}
+						}}
+						>Submit</button>
+					</form>	
+			}				
+		</div>		
+	);
 }
