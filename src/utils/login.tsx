e7ddf5700538e81b53915f65d529d9fdbddbadb6
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-11 14:58:49 +0400

    FIX: login parameters implementation

--- src/utils/login.tsx
+++ src/utils/login.tsx
@@ -1,5 +1,5 @@
 export const login = async(login: string, password: string) => {
-	const res : object | null = await fetch("http://localhost:5000/api/v1/User/?" + new URLSearchParams({login: login, password: password, repeatedPassword: repeatedPassword}), {method: 'POST'})
+	const res : object | null = await fetch("http://localhost:5000/api/v1/User/?" + new URLSearchParams({login: login, password: password}), {method: 'GET'})
 		.then(response => response.text())
 		.then(response => {
 			if (response === "true") {
