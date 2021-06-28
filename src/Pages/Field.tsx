52ddb1e5b6bbea603764f11bf13c90accaf8c23e
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-06-28 19:00:17 +0300

    ADD: Field delete/patch dummies

--- src/Pages/Field.tsx
+++ src/Pages/Field.tsx
@@ -3,6 +3,8 @@ import React, {useContext} from 'react';
 import {UserContext} from "../UserContext.tsx";
 // @ts-ignore
 import FieldForm from '../Components/FieldForm.tsx';
+// @ts-ignore
+import FieldsList from '../Components/FieldsList.tsx';
 //@ts-ignore
 import Main from "./Main.tsx";
 
@@ -13,7 +15,9 @@ export function Field() {
 	{user ?
 			<div>
 				<h2>Fields page</h2>
+				<FieldsList /><button id="editField">Изменить поле</button><button id="deleteField">Удалить поле</button>
 				<FieldForm />
+				<button id="changeField">Внести изменения в базу данных</button><br />
 			</div>
 			:
 			<Main />}</div>);
