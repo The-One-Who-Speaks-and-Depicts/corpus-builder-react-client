e305b43d93b523119b36899efc8e259749d999c4
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-11 16:24:13 +0400

    ADD: value choice reaction dummy for field connector

--- src/Components/FieldConnections.tsx
+++ src/Components/FieldConnections.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { convertTypeAcquisitionFromJson } from 'typescript';
 
 class FieldConnections extends React.Component {
 
@@ -7,6 +8,29 @@ class FieldConnections extends React.Component {
 			isLoaded: false,
 			items: ""		
 		}
+	
+	
+	valueChoice = async() => {
+		let fieldMother : HTMLSelectElement = document.getElementById("fieldMother") as HTMLSelectElement;
+		let valueMother : HTMLSelectElement = document.getElementById("valueMother") as HTMLSelectElement;
+		let selectedKey : string = fieldMother.options[fieldMother.selectedIndex].text;
+		let selectedValue : string = valueMother.options[valueMother.selectedIndex].text;
+		if (selectedValue !== "Any")
+		{
+			let checkboxes : NodeListOf<Element> = document.querySelectorAll('#fieldConnector input');
+			for (let i = 0; i < checkboxes.length; i++)
+			{
+				(checkboxes[i] as HTMLInputElement).disabled = false;
+				(checkboxes[i] as HTMLInputElement).checked = false;
+				if (checkboxes[i].id === selectedKey)
+				{
+					(checkboxes[i] as HTMLInputElement).disabled = true;
+					(checkboxes[i] as HTMLInputElement).checked = false;
+				}
+				
+			}
+		}		
+	}
 
 	componentDidMount() {
 	fetch("http://localhost:5000/api/v1/Field/")
@@ -23,7 +47,7 @@ class FieldConnections extends React.Component {
 
 	async fieldChoice() {
 		if (this.state.isLoaded){
-			(document.getElementById("valueMother") as HTMLSelectElement).innerHTML = "";
+			(document.getElementById("valueMother") as HTMLSelectElement).innerHTML = "<option>Any</option>";
 			const selectedFieldValues = await fetch("http://localhost:5000/api/v1/Field/one?name=" + (document.getElementById("fieldMother")! as HTMLSelectElement).options[(document.getElementById("fieldMother")! as HTMLSelectElement).selectedIndex].value)
 				.then(response => response.text());
 			let vals = JSON.parse(selectedFieldValues).values.map((t : any) => t);
@@ -41,11 +65,12 @@ class FieldConnections extends React.Component {
 		<div>
 		{!isLoaded && <div>Loading fields...</div>}		
 		{isLoaded && !error &&
-			<div>
+			<div id="fieldConnector">
 				<select id="fieldMother" onChange={() => this.fieldChoice()}>
+					<option>Any</option>
 					{options}
 				</select>
-				<select id="valueMother">
+				<select id="valueMother" onChange={() => this.valueChoice()}>
 					<option>Any</option>
 				</select> <br />
 				{checkboxes}			
